f1a8204d03ff952e4806c1f889fc0ef331ba8d29
frvnci5 <frvncis@outlook.com>
2023-08-28 23:17:02 -0700

    got create note working

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,33 +1,50 @@
-const router = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const db = require('../db/db.json');
-const { createNewNote, findById, editNote, removeNote, readFromFile } = require('../lib/notes');
-const fakeNote = {a:'hi',b:'loser'};
+const router = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const db = require("../db/db.json");
+const {
+  createNewNote,
+  findById,
+  editNote,
+  removeNote,
+  readFromFile,
+} = require("../lib/notes");
+const fakeNote = { a: "hi", b: "loser" };
 
-router.get('/notes', async (req, res) => {
+router.get("/notes", async (req, res) => {
+  const notes = await readFromFile("./db/db.json");
+  const parsedNotes = JSON.parse(notes);
+  // readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  return res.json(parsedNotes);
+});
+
+router.post("/notes", async (req, res) => {
+  const { title, text } = req.body;
+
+  if ((title, text)) {
     const notes = await readFromFile("./db/db.json");
-    const parsedNotes = JSON.parse(notes)
-    // readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-    return res.json(parsedNotes);
+    const parsedNotes = JSON.parse(notes);
+
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    //add new note to notes array
+  parsedNotes.push(newNote);
+  console.log(parsedNotes)
+   createNewNote(newNote,parsedNotes);
+  }
+  const finNotes = await readFromFile("./db/db.json");
+    return res.json(finNotes)  
+
 });
 
-router.post('/notes', (req, res) => {
+router.delete("/notes/:id", (req, res) => {
+  const note = findById(req.params.id, notes);
 
-    if (!req.body.id) {
-        req.body.id = uuidv4();
-        createNewNote(req.body, notes);
-    } else {
-        editNote(req.body, notes);
-    }
-
-    res.json(req.body);
-});
-
-router.delete('/notes/:id', (req, res) => {
-    const note = findById(req.params.id, notes);
-
-    removeNote(note, notes);
-    res.json();
+  removeNote(note, notes);
+  res.json();
 });
 
 module.exports = router;
