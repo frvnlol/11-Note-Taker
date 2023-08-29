013608fa4ef2d87ded785081fc4c5af3f42b1b69
frvnci5 <frvncis@outlook.com>
2023-08-28 22:20:19 -0700

    readme changes

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
-
+const path = require('path');
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+
+
 
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT} 🚀`)
